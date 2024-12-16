50eda8865f35aa98520678d774517751f954f96d
Sylvain Lesage <severo@rednegra.net>
2024-12-16 23:42:13 +0100

    lint provider

--- packages/components/src/lib/provider.tsx
+++ packages/components/src/lib/provider.tsx
@@ -1,41 +1,48 @@
+import { ThemeProviderProps } from '@theme-ui/core'
 import { useThemedStylesWithMdx } from '@theme-ui/mdx'
 import Prism from '@theme-ui/prism'
-import type { ComponentPropsWithoutRef } from 'react'
+import type { ComponentPropsWithoutRef, JSX, ReactNode } from 'react'
 import { Card, Heading, Paragraph, ThemeUIProvider } from 'theme-ui'
 import CodeBlock, { InlineCode } from '../components/CodeBlock.js'
 import Link from '../components/Link.js'
 
-function createLinks({ ...props }) {
-  return <Link href={props.href}>{props.children}</Link>
+function createLink(props: JSX.IntrinsicElements['a']) {
+  const { href } = props
+  return href ? <Link href={href}>{props.children}</Link> : props.children
 }
 
-const createHeadingWithLink =
-  (Level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6') =>
-    (props: ComponentPropsWithoutRef<typeof Heading>) =>
-
-      <Heading as={Level} {...props}>
+function createHeadingWithLink(level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6') {
+  return function headingWithLink(props: ComponentPropsWithoutRef<typeof Heading>) {
+    return (
+      <Heading as={level} {...props}>
         {props.children}
-      </Heading>
+      </Heading>)
+  }
+}
 
-export const components = {
+const components = {
   h1: createHeadingWithLink('h2'),
   h2: createHeadingWithLink('h3'),
   h3: createHeadingWithLink('h4'),
   h4: createHeadingWithLink('h5'),
   h5: createHeadingWithLink('h6'),
   h6: createHeadingWithLink('h6'),
-  a: createLinks,
-  p: ({ children }) => <Paragraph>{children}</Paragraph>,
-  blockquote: ({ children }) => <Card variant="blockquote">{children}</Card>,
-  code: ({ children, className }) => {
-    return children.indexOf('\n') == -1 ?
-      <InlineCode>
-        <Prism children={children} className={className} />
-      </InlineCode>
-      :
+  a: createLink,
+  p: ({ children }: { children?: ReactNode }) => <Paragraph>{children}</Paragraph>,
+  blockquote: ({ children }: { children?: ReactNode }) => <Card variant="blockquote">{children}</Card>,
+  code: ({ children, className = '' }: { children?: ReactNode, className?: string }) => {
+    if (typeof children !== 'string') {
+      /// only implemented if children is a string
+      return <></>
+    }
+    return children.includes('\n') ?
       <CodeBlock copyButton={true}>
-        <Prism children={children} className={className} />
+        <Prism className={className}>{children}</Prism>
       </CodeBlock>
+      :
+      <InlineCode>
+        <Prism className={className}>{children}</Prism>
+      </InlineCode>
 
   },
 }
@@ -44,6 +51,6 @@ export function SourceComponents() {
   return useThemedStylesWithMdx(components)
 }
 
-export function SourceProvider({ children, theme }) {
+export function SourceProvider({ children, theme }: {children?: ReactNode, theme: ThemeProviderProps['theme']}) {
   return <ThemeUIProvider theme={theme}>{children}</ThemeUIProvider>
 }
