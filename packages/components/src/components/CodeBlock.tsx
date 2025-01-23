853f08f16c26fc91e15620183ded38a3b55fffaa
Sylvain Lesage <severo@rednegra.net>
2025-01-23 14:33:34 +0100

    fix copy button

--- packages/components/src/components/CodeBlock.tsx
+++ packages/components/src/components/CodeBlock.tsx
@@ -31,35 +31,35 @@ export default function CodeBlock({ children, copyButton, ...props }: CodeBlockP
   const copy = useCopyToClipboard()[1]
 
   const buttonRef = useRef<HTMLElement | undefined>(undefined)
-  const { current } = buttonRef
   return (
     <Card variant="code" {...props}>
-      {copyButton && current &&
-        <Box
-          sx={{
-            pl: 1,
-            pb: 1,
-            width: '19px',
-            height: '19px',
-            position: 'absolute',
-            right: 1,
-            top: 1,
-            zIndex: 999,
-            cursor: 'pointer',
-            backgroundColor: 'muted',
-          }}
-          onClick={() => {
-            copy(copyToClipboard(current)).catch((e: unknown) => { console.error(e) })
-          }}
+      {copyButton && <Box
+        sx={{
+          pl: 1,
+          pb: 1,
+          width: '19px',
+          height: '19px',
+          position: 'absolute',
+          right: 1,
+          top: 1,
+          zIndex: 999,
+          cursor: 'pointer',
+          backgroundColor: 'muted',
+        }}
+        onClick={() => {
+          if (buttonRef.current) {
+            copy(copyToClipboard(buttonRef.current)).catch((e: unknown) => { console.error(e) })
+          }
+        }}
+      >
+        <SVG
+          viewBox="0 0 16 16"
+          sx={{ fill: 'primary', '&:hover': { fill: 'secondary' } }}
         >
-          <SVG
-            viewBox="0 0 16 16"
-            sx={{ fill: 'primary', '&:hover': { fill: 'secondary' } }}
-          >
-            <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
-            <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
-          </SVG>
-        </Box>
+          <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+          <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+        </SVG>
+      </Box>
       }
       <Box ref={buttonRef}>{children}</Box>
     </Card>
