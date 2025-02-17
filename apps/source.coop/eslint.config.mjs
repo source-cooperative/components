c40c61832034d7f73a3be21a800e4d618ce872ec
Sylvain Lesage <severo@rednegra.net>
2025-02-17 11:55:15 +0100

    setup minimal eslint + disable typecheck for now

--- apps/source.coop/eslint.config.mjs
+++ apps/source.coop/eslint.config.mjs
@@ -1,46 +1,14 @@
-import js from '@eslint/js'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import { sharedJsReactRules, sharedJsRules, sharedTsRules } from '../../eslint.config.mjs'
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default tseslint.config(
-  {
-    settings: { react: { version: '18.3' } },
-    extends: [js.configs.recommended, ...tseslint.configs.strictTypeChecked, ...tseslint.configs.stylisticTypeChecked],
-    files: ['**/*.{ts,tsx,js}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {...globals.node, ...globals.browser},
-      parserOptions: {
-        project: './tsconfig.eslint.json',
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      'react': react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.recommended.rules,
-      ...sharedJsRules,
-      ...sharedJsReactRules,
-      ...sharedTsRules,
-    },
-  },
-  {
-    files: ['**/*.js', '**/*.mjs'],
-    ...tseslint.configs.disableTypeChecked,
-  },
-)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+const config = [...compat.extends("next/core-web-vitals", "next/typescript")];
+export default config
